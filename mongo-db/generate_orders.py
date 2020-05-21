3e2fab8051e157eed79d39702bae445cbd08608e
Stephan-MDD <stephan.mdd@gmail.com>
2020-05-21 16:39:24 +0200

    fast commit

--- mongo-db/generate_orders.py
+++ mongo-db/generate_orders.py
@@ -14,7 +14,6 @@ print(f'generating {amounts} orders')
 ####### DATABASE CONNECTIONS #######
 ####################################
 
-
 def neo4j_query_get_items():
     driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "1234"))
     with driver.session() as session:
@@ -29,17 +28,8 @@ def neo4j_query_get_items():
 ##### DATABASE CONNECTIONS END #####
 ####################################
 
-
-
 results = neo4j_query_get_items()
-
-
-# for item in results:
-#     if item['item'].id == 27:
-#         print(item['item']['name'])
-
 formatted_items = [{'id': record['item'].id, 'name': record['item']['name'], 'price': record['item']['price']} for record in results]
-
 
 def random_item_list():
     types = randint(1, 10)
@@ -63,20 +53,18 @@ def random_item_list():
 
     return result
 
-
 # generating data
 for n in range(amounts):
     items = random_item_list()
 
     order = {
-        'date': int(datetime.now().timestamp()) - randint(0, 2629743),  # maybe as random? => (datetime.now() + timedelta(days=30)).timestamp()
+        'date': int(datetime.now().timestamp()) - randint(0, 2629743),
         'user_id': randint(1, 10_000),
         'items': items,
-        'discount': None,   # needs implementation
+        'discount': None,
     }
 
     orders.append(order)
-
 
 # saving to .json
 with open('mongo-db/data/orders.json', 'w') as fp:
